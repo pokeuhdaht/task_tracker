9198bc7c6fd3d71211d6b41ef1912909857d198f
pokeuhdaht <gavyn.pitale@gmail.com>
2026-09-24 16:50:55 -0400

    added a "help" command and a few other functions that would be used in main()

--- tracker.py
+++ tracker.py
@@ -1,13 +1,34 @@
 import json
 
+#create a task
+def addTask():
+    print("add task")
+
+#update a specific task as defined by the user
+def updateTask():
+    print("update task")
+
+#delete the task as created by the user
+def deleteTask():
+    print("delete task")
+
+
+
 def main():
+    print("Task Tracker")
+    print("Enter 'help' for assistance.")
     looping = True
     while looping == True:
         userInput = input("> ")
-        print(userInput)
+        userInput = userInput.lower()
         if userInput.lower()=='exit':
             looping = False
-        
+        elif userInput.lower()=='add':
+            addTask()
+        elif userInput=='help':
+            print(f"add\nUsed to add a new task to the tracker.")
+        else:
+            print("not an available command")
 
 if __name__=="__main__":
     main()
